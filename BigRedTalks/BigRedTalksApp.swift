8c72bca5207fa3f069a4b4ad49756efe11c156a2
Connor Chen <cjc353@cornell.edu>
2021-12-20 12:58:32 -0500

    Initial Commit

---
+++ BigRedTalks/BigRedTalksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigRedTalksApp.swift
+//  BigRedTalks
+//
+//  Created by Fam Bam on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct BigRedTalksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
