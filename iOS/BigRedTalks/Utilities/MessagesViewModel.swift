9bd237cc5f3d8458b78b4dbb9602962443d80f8d
Connor Chen <cjc353@cornell.edu>
2022-01-16 12:37:18 -0500

    did backend setup but not tested

--- iOS/BigRedTalks/Utilities/MessagesViewModel.swift
+++ iOS/BigRedTalks/Utilities/MessagesViewModel.swift
@@ -6,21 +6,77 @@
 //
 
 import Foundation
-import Firebase
-
 
 struct Message: Codable, Identifiable {
-    var id: String
-    var content: String
-    var name: String
+    var id: String?
+    var senderEmail: String
+    var username: String
+    var text: String
+    var timestamp: String
+    var likes: [String]
 }
 
 class MessagesViewModel: ObservableObject {
     @Published var messages = [Message]()
-    private let user = Auth.auth().currentUser
-    private let db = ""
+    private let baseUrl = "http://bigredtalks.herokuapp.com/"
     
-    func sendMessage (messageContent: String, docId: String) {
+    func getMessages (messageIndex: Int) {
+        guard let url = URL(string: baseUrl + "messages/" + String(messageIndex)) else { return }
         
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            self.messages = try! JSONDecoder().decode([Message].self, from: data!)
+        }
+        .resume()
     }
+    
+    func sendMessage (messageSenderEmail: String, messageUsername: String, messageText: String) {
+        guard let url = URL(string: baseUrl + "messages/add") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body: [String: AnyHashable] = [
+            "senderEmail": messageSenderEmail,
+            "username": messageUsername,
+            "text": messageText
+        ]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let response = try JSONSerialization.data(withJSONObject: data, options: .fragmentsAllowed)
+                print(response)
+            } catch {
+                print(error)
+            }
+        }
+        .resume()
+    }
+    
+    func editLikes (messageId: String, messageLikes: [String]) {
+        guard let url = URL(string: baseUrl + messageId) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body: [String: AnyHashable] = [
+            "likes": messageLikes
+        ]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let response = try JSONSerialization.data(withJSONObject: data, options: .fragmentsAllowed)
+                print(response)
+            } catch {
+                print(error)
+            }
+        }
+        .resume()
+    }
+
 }
